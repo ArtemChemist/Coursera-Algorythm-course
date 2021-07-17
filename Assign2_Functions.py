c08da045e257b3eae9a4e381ccfe6d3e3d80f3ef
Artem <lebedevfedora@gmail.com>
2021-07-17 20:22:28 +0300

    Fixed indexing solution

--- Assign2_Functions.py
+++ Assign2_Functions.py
@@ -1,20 +1,22 @@
-def PairwiseProduct(numbers):
+def PairwiseProduct(numbers: list):
     result = 0
     for i in numbers:
+        ind_i = numbers.index(i)
         for k in numbers:
-            if (i != k and i*k>result):
+            ind_k = numbers.index(k)
+            if (ind_i != ind_k and i*k>result):
                 result = i*k
     return result
 
-def PairwiseProductFast(numbers):
+def PairwiseProductFast(numbers: list):
     largest = 0
     largest_ind = 0
     for i in numbers:
         if (i>largest):
             largest = i
-            largest_ind = list.index(i)
+            largest_ind = numbers.index(largest)
     second_largest = 0
     for k in numbers:
-        if (k>second_largest and list.index(k) != largest_ind):
-            second_largest  = i
+        if (k>second_largest and numbers.index(k) != largest_ind):
+            second_largest  = k
     return second_largest *largest  
--- Assign2_Functions_test.py
+++ Assign2_Functions_test.py
@@ -6,9 +6,8 @@ import random
 
 class TestFastOne(unittest.TestCase):
     def test_twonumbers(self):
-        n = random.randint(2,5)
-        randomlist = random.sample(range(0, 10), n)
-        result = True
-        while result:
-            result = self.assertEqual(Assign2_Functions.PairwiseProductFast(randomlist), Assign2_Functions.PairwiseProduct(randomlist))
-            print(result)
+        n = random.randint(2,11)
+        randomlist = random.sample(range(0, 99999), n)
+        print(randomlist)
+        self.assertEqual(Assign2_Functions.PairwiseProductFast(randomlist), Assign2_Functions.PairwiseProduct(randomlist))
+
