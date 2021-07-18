14bb62122c7685f9ebbbe92b4ec267937e4ec74e
Artem <lebedevfedora@gmail.com>
2021-07-18 18:23:52 +0300

    refactoring

--- week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,11 +1,11 @@
 # Uses python3
 def calc_fib(n):
-    F = []
     if (n == 0):
         return 0
+    F = []
     F.append(0)
     F.append(1)
-    for i in range(2,n):
+    for i in range(2,n+1):
         F.append(F[i-1]+F[i-2])
     return F[-1]
 
