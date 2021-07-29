72617f8ec388d50a11d0456b0cebaaec18db3ffd
Artem <lebedevfedora@gmail.com>
2021-07-29 13:06:56 +0300

    Digital knapsack

--- week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,13 +1,22 @@
 # Uses python3
 import sys
+import numpy as np
 
 def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    w = [0]+w[:]
+    sln = np.full((len(w),W+1), 0, dtype = int)
+    for row in range (1,len(w)):
+        for clmn in range (1, W+1):
+            #print(sln)
+            sln[row,clmn] = sln[row-1, clmn]
+            if (w[row]<=clmn):
+                no_add = sln[row, clmn]
+                add = sln[row-1, clmn-w[row]]+w[row]
+                sln[row,clmn] = max (add, no_add )
+                #print(sln)
+                #print('------------')
+
+    return sln[-1,-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
